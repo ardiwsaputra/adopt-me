f9d7827390912d08ff513acd84bb382d0dfd3cc3
Ardi Widyanto Saputra <ardiwidyanto15@gmail.com>
2019-09-03 22:09:59 +0700

    add npm script, eslint, prettier, gitignor, Parcel Build Process. Extract out Pet component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-const Pet = ({
-    name,
-    animal,
-    breed
-}) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-
-    ])
-}
+import React from 'react';
+import {
+    render
+} from 'react-dom';
+import Pet from './Pet';
 
 const App = () => {
     return React.createElement("div", {}, [
@@ -20,7 +13,7 @@ const App = () => {
             breed: "Persia"
         }),
         React.createElement(Pet, {
-            name: "Pepper",
+            name: "Catalan",
             animal: "Bird",
             breed: "Cocktaiel"
         }),
@@ -32,5 +25,4 @@ const App = () => {
     ])
 }
 
-ReactDOM.render(
-    React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
